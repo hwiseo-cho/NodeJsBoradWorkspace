958949d40845bcebca4499993e573375e4acea6d
eunchae-Cho <gnltj37@naver.com>
2020-12-24 17:21:32 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer.js';
+
+const customers = [{
+  'id': 1,
+  'image':'http://placeimg.com/64/64/1',
+  'name':'조휘서',
+  'birthday':'980716',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id': 2,
+  'image':'http://placeimg.com/64/64/2',
+  'name':'홍길동',
+  'birthday':'930302',
+  'gender':'남자',
+  'job':'의사'
+},
+{
+  'id': 3,
+  'image':'http://placeimg.com/64/64/3',
+  'name':'박동구',
+  'birthday':'000430',
+  'gender':'남자',
+  'job':'운동선수'
+}]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's Develop Management System!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              id = {c.id}
+              image = {c.image}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}
+            />
+          );
+        })
+      }
     </div>
   );
 }
