2dd6a21f030edca67ad7101a89baf05b024211f5
eunchae-Cho <gnltj37@naver.com>
2020-12-25 18:33:05 +0900

    commit

---
+++ client/src/App.js
@@ -0,0 +1,81 @@
+import logo from './logo.svg';
+import './App.css';
+import Customer from './components/Customer.js';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root:{
+    width:'100%',
+    marginTop: theme.spacing.unit*3,
+    overflowX: "auto"
+  },
+  table:{
+    minWidth: 1080
+  }
+})
+
+const customers = [{
+  'id': 1,
+  'image':'http://placeimg.com/64/64/1',
+  'name':'조휘서',
+  'birthday':'980716',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id': 2,
+  'image':'http://placeimg.com/64/64/2',
+  'name':'홍길동',
+  'birthday':'930302',
+  'gender':'남자',
+  'job':'의사'
+},
+{
+  'id': 3,
+  'image':'http://placeimg.com/64/64/3',
+  'name':'박동구',
+  'birthday':'000430',
+  'gender':'남자',
+  'job':'운동선수'
+}]
+
+function App() {
+  return (
+    <Paper className={styles.root}>
+        <Table className={styles.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {
+              return (
+                <Customer
+                  id = {c.id}
+                  image = {c.image}
+                  name = {c.name}
+                  birthday = {c.birthday}
+                  gender = {c.gender}
+                  job = {c.job}
+                />
+              );
+            })}
+          </TableBody>
+        </Table>
+    </Paper>
+  );
+}
+
+export default withStyles(styles)(App);
